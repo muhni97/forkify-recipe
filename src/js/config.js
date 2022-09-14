2da5e131498bc7170615008a7570aa8aa8a39aea
Muhni <mstfsrgl5458@gmail.com>
2022-09-14 10:51:53 +0300

    Initial

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10
+export const KEY = '947cfcc6-3585-4873-9757-e8d7d796350c'
+export const MODAL_CLOSE_SEC = 2.5
