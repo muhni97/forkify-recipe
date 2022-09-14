618fea1fa1f2489c52438c3e231fd2c4316b007e
Muhni <mstfsrgl5458@gmail.com>
2022-09-14 11:04:52 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -133,6 +133,10 @@ const controlAddRecipe = async function(newRecipe){
 
 }
 
+const newFeature = function(){
+  console.log('welcome to the app')
+}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks)
   recipeView.addHandlerRender(controlRecipes);
@@ -141,6 +145,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe)
-  console.log('Welcome')
+  newFeature()
 };
 init();
